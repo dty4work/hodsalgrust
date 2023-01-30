f90306d9cde3e8bdbfe6da39920a181407b38f8b
dty4work <121413977+dty4work@users.noreply.github.com>
2023-01-30 21:40:41 +0000

    finish str vs String

--- 01/v8_box/src/main.rs
+++ 01/v8_box/src/main.rs
@@ -35,4 +35,36 @@ fn main() {
         v.push(i.to_string());
     }
     println!("v.len = {}, v.capacity = {}", v.len(), v.capacity());
+
+    let mut s = "   hello   ".to_string();
+    let p = s.trim().to_string();
+
+    s.push_str("goodbyte");
+
+    println!("p = {}", p);
+    println!("s = {}", s);
+
+    let fstr = "help me find home";
+    let ffstr = string_find_f(fstr);
+    println!("ffstr = '{}'", ffstr);
+    println!("choosen = '{}'", choose_str(1));
+}
+
+fn string_find_f<'a>(s: &'a str) -> &'a str {
+    // Box<str> === String
+    let n = 0;
+    for (n, x) in s.char_indices() {
+        if x == 'f' {
+            return &s[n..];
+        }
+    }
+    s
+}
+
+fn choose_str(n: i32) -> &'static str {
+    match n {
+        0 => "hello",
+        1 => "goodbye",
+        _ => "other",
+    }
 }
