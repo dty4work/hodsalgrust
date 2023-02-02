a10d680aad5f16761c8eae24eb4057e9b87d3e72
dty4work <121413977+dty4work@users.noreply.github.com>
2023-02-02 02:35:51 +0000

    add random number generator, lazy static, and randomized quick sort 0 based

--- 02/v1-sorting/src/lib.rs
+++ 02/v1-sorting/src/lib.rs
@@ -1,5 +1,6 @@
 use std::fmt::Debug;
 
+mod b_rand;
 // O(n^2)
 pub fn bubble_sort<T: PartialOrd + Debug>(v: &mut [T]) {
     for p in 0..v.len() {
@@ -17,6 +18,92 @@ pub fn bubble_sort<T: PartialOrd + Debug>(v: &mut [T]) {
     }
 }
 
+pub fn merge_sort<T: PartialOrd + Debug>(mut v: Vec<T>) -> Vec<T> {
+    // Sort the left half,  O(n * ln(n))
+    // Sort the right half, O(n * ln(n))
+    // bring the sorted halfs together O(n)
+
+    println!("MS:{:?}", v);
+    if v.len() <= 1 {
+        return v;
+    }
+
+    let mut res: Vec<T> = Vec::with_capacity(v.len());
+    let b = v.split_off(v.len() / 2);
+    let a = merge_sort(v);
+    let b = merge_sort(b);
+
+    // bring them together
+    let mut a_it = a.into_iter();
+    let mut b_it = b.into_iter();
+    let mut a_peek = a_it.next();
+    let mut b_peek = b_it.next();
+
+    loop {
+        match a_peek {
+            Some(ref a_val) => match b_peek {
+                Some(ref b_val) => {
+                    if b_val < a_val {
+                        res.push(b_peek.take().unwrap());
+                        b_peek = b_it.next();
+                    } else {
+                        res.push(a_peek.take().unwrap());
+                        a_peek = a_it.next();
+                    }
+                }
+                None => {
+                    res.push(a_peek.take().unwrap());
+                    res.extend(a_it);
+                    return res;
+                }
+            },
+            None => {
+                if let Some(b_val) = b_peek {
+                    res.push(b_val);
+                }
+                res.extend(b_it);
+                return res;
+            }
+        }
+    }
+}
+
+// Move first element to the correct place
+// Everything lower should be before it
+// everything higher should be after it
+// return it's location
+pub fn pivot<T: PartialOrd + Debug>(v: &mut [T]) -> usize {
+    // let mut p = 0;
+
+    let mut p = b_rand::rand(v.len());
+    v.swap(p, 0);
+
+    p = 0;
+    for i in 1..v.len() {
+        // println!("PV: i = {} p = {} v = {:?}", i, p, v);
+        if v[i] < v[p] {
+            // move our pivot forward 1, and put this element before it
+            v.swap(p + 1, i);
+            v.swap(p, p + 1);
+            p += 1
+        }
+    }
+    // println!("PV: p = {}, v = {:?}", p, v);
+    p
+}
+
+pub fn quick_sort<T: PartialOrd + Debug>(v: &mut [T]) {
+    if v.len() <= 1 {
+        return;
+    }
+    let p = pivot(v);
+    println!("{:?}", v);
+
+    let (a, b) = v.split_at_mut(p);
+    quick_sort(a);
+    quick_sort(&mut b[1..]);
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -25,21 +112,40 @@ mod tests {
     fn test_buble_sort() {
         let mut v = vec![4, 6, 1, 8, 11, 13, 3];
         bubble_sort(&mut v);
+        // assert_eq!(v, vec![1, 3, 4, 6, 8, 11, 13])
         assert_eq!(v, vec![1, 3, 4, 6, 8, 11, 13])
     }
+
+    #[test]
+    fn test_merge_sort() {
+        let v = vec![4, 6, 1, 8, 11, 13, 3];
+        let v = merge_sort(v);
+        assert_eq!(v, vec![1, 3, 4, 6, 8, 11, 13])
+        // assert_eq!(v, vec![3, 4, 6, 8, 11, 13]);
+    }
+
+    #[test]
+    fn test_pivot() {
+        let mut v = vec![4, 6, 1, 19, 8, 11, 13, 3];
+        let p = pivot(&mut v);
+
+        for x in 0..v.len() {
+            assert!((v[x] < v[p]) == (x < p));
+        }
+
+        // assert_eq!(v, vec![1, 3, 4, 6, 19, 8, 11, 13]);
+        assert_eq!(v, vec![1, 3, 4, 19, 8, 11, 13, 6]);
+    }
+
+    #[test]
+    fn test_quick_sort() {
+        let mut v = vec![4, 6, 1, 8, 11, 13, 3];
+        quick_sort(&mut v);
+        assert_eq!(v, vec![1, 3, 4, 6, 8, 11, 13]); // Expected
+
+        let mut v = vec![1, 2, 6, 7, 9, 12, 13, 14];
+        quick_sort(&mut v);
+        assert_eq!(v, vec![1, 2, 6, 7, 9, 12, 13, 14]);
+        panic!();
+    }
 }
-
-// pub fn add(left: usize, right: usize) -> usize {
-//     left + right
-// }
-
-// #[cfg(test)]
-// mod tests {
-//     use super::*;
-
-//     #[test]
-//     fn it_works() {
-//         let result = add(2, 2);
-//         assert_eq!(result, 4);
-//     }
-// }
