602c2121844ff5fa97019f91edaef73d4f4c0148
dty4work <121413977+dty4work@users.noreply.github.com>
2023-02-12 02:41:46 +0000

    refactor ending to ifelse expr

--- rcli/02_echor/src/main.rs
+++ rcli/02_echor/src/main.rs
@@ -24,10 +24,7 @@ fn main() {
     let text = matches.values_of_lossy("text").unwrap();
     let omit_newline = matches.is_present("omit_newline");
 
-    let mut ending = "\n";
-    if omit_newline {
-        ending = ""; // This will not work
-    }
+    let ending = if omit_newline { "" } else { "\n" };
     print!("{}{}", text.join(" "), ending);
 
     // println!("{:#?}", matches);
